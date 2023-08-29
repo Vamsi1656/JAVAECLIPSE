734eba1bcd611bf522c6790bc2c68c1bfc00930e
Vamsi1656 <vamsireddy1717@gmail.com>
2023-08-29 18:59:05 +0530

    JAVA

--- src/com/Strings/Test3.java
+++ src/com/Strings/Test3.java
@@ -3,23 +3,34 @@ package com.Strings;
 public class Test3 {
 
 	public static void main(String[] args) {
-		String s1="HeLlo";
+		String s1="  HeLlo  ";
 		String s2="hello";
 		String s3="HELLO";
 		
-		System.out.println(s1.equals(s2)); // It is false because it compares as it is String including case sensitive. 
-		System.out.println(s1.equalsIgnoreCase(s2)); // It is true because if it data is same irrespective of cases 
-													 // it give true.
-		
-		System.out.println(s3.contains("HE")); // It is true because it contains the text if any character changes it 
-		                                       // cases it shows false.
+//		System.out.println(s1.equals(s2)); // It is false because it compares as it is String including case sensitive. 
+//		System.out.println(s1.equalsIgnoreCase(s2)); // It is true because if it data is same irrespective of cases 
+//													 // it give true.
 //		
+//		System.out.println(s3.contains("HE")); // It is true because it contains the text if any character changes it 
+//		                                       // cases it shows false.
+////		
 //		System.out.println(s3.contains("he")); // Here HE is there but upper case we give lower case that's why it shows 
 //											   // false.
 		
 //		System.out.println(s1.startsWith("He"));
 //		
 //		System.out.println(s1.endsWith("Llo"));
+		
+		System.out.println(s1.trim()); // It removes the white spaces in starting and ending.
+		
+		
+		System.out.println(s1.length());  // Length method shows how many characters are in the string.
+		System.out.println(s1.trim().length());  // It counts white spaces also.
+		
+		
+		
+		
+
 	}
 
 }
