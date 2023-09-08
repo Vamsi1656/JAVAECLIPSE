e5e2ed28130e4c5f8966610ad3d28fd1fcd49fa1
Vamsi1656 <vamsireddy1717@gmail.com>
2023-09-08 11:21:55 +0530

    JAVA

--- src/com/Practice/Test5.java
+++ src/com/Practice/Test5.java
@@ -7,16 +7,20 @@ public class Test5 {
 	public static void main(String[] args) {
 		 Vector v1=new Vector();
 		   
-		   v1.add(1);
-		   v1.add(2);
-		   v1.add(3);
+		   v1.add("Vamsi");
+		   v1.add("Nani");
+		   v1.add("Virat");
+		   v1.add(0,"Pallavi");
 		   
-		   for(Object v:v1) {
-			   
-			   System.out.println(v);
-			   
+		   for(Object v2:v1)
+		   {
+			   System.out.println(v2);
+			     
 		   }
-
+		   
+		  
+		  
+		
 	}
 
 }
--- src/com/collections/VectorEx.java
+++ src/com/collections/VectorEx.java
@@ -9,28 +9,29 @@ public class VectorEx {
 	   
 	   v1.add("Vasu Anna");
 	   v1.add("Naveen Anna");
-	   v1.add(0,"Nani");
-	   v1.add(1);
-	   v1.add(4.2);
-	   v1.add("Vasu Anna");
-	   v1.add("Naveen Anna");
-	   v1.add(0,"Nani");
-	   v1.add(1);
-	   v1.add(4.2);
+	   v1.add("Nani");
 	   
-	   v1.add("Virat");
+	   System.out.println(v1);
+       Vector v2=new Vector();
+	   
+	   v2.add("Pallavi");
+	   v2.add("Virat");
+	   
+	     
+//	   for(int i=0;i<v2.size();i++) {
+//		   v1.add(v2.get(i));
+//	   }
+	   
+	   v1.addAll(v2);
+	  
 	   
 	   
-	   for(Object v2:v1)
-	   {
-		   System.out.println(v2);
-		     
-	   }
-	   
+//	   System.out.println("Size:"+v1.size());  // No.of elements in an Array
+//	   System.out.println("Capacity:"+v1.capacity()); // Total number of size like in this program total size is 40.
+//	   												  // 40 is the capacity.
 	 
-	   
-//	   
-//	   System.out.println(v1);
+	      System.out.println(v1);
+
 
 	}
 
