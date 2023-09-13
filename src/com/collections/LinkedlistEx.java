3912ced54281df060664c503e2da37513b3ae6b4
Vamsi1656 <vamsireddy1717@gmail.com>
2023-09-13 11:22:49 +0530

    JAVA

---
+++ src/com/collections/LinkedlistEx.java
@@ -0,0 +1,20 @@
+package com.collections;
+
+import java.util.*;
+
+public class LinkedlistEx {
+
+	public static void main(String[] args) {
+//		List<String> ll=new LinkedList();
+//		Deque<String> ll=new LinkedList();
+//		Queue<String> ll=new LinkedList();
+		
+		LinkedList<String> ll=new LinkedList<>();
+		ll.add("Nani");
+		ll.add("Virat");
+		ll.add("Pallavi");
+		System.out.println(ll);
+
+	}
+
+}
